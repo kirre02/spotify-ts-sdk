22fd587856306afc4e2dc9bfa54a99db51131d64
voffie <viktorsoderborg@gmail.com>
2025-01-30 17:18:30 +0100

    feat(types): add Show types

--- src/types.ts
+++ src/types.ts
@@ -249,6 +249,39 @@ type SavedEpisode = {
   episode: Episode;
 };
 
+type SimplifiedShow = {
+  available_markets: string[];
+  copyrights: Copyright[];
+  description: string;
+  html_description: string;
+  explicit: boolean;
+  external_urls: ExternalUrls;
+  href: string;
+  id: string;
+  images: Image[];
+  is_externally_hosted: boolean;
+  languages: string[];
+  media_type: string;
+  name: string;
+  publisher: string;
+  type: string;
+  uri: string;
+  total_episodes: number;
+};
+
+type Show = SimplifiedShow & {
+  episodes: Page<SimplifiedEpisode>;
+};
+
+type Shows = {
+  shows: Show[];
+};
+
+type SavedShow = {
+  added_at: string;
+  show: SimplifiedShow;
+};
+
 type SimplifiedTrack = {
   artists: SimplifiedArtist[];
   available_markets: string[];
