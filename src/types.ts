87c8abe10a86a56792c86ec341978c9da728103e
voffie <viktorsoderborg@gmail.com>
2025-01-30 17:42:51 +0100

    feat(types): add Track types

--- src/types.ts
+++ src/types.ts
@@ -285,20 +285,27 @@ type SavedShow = {
 type SimplifiedTrack = {
   artists: SimplifiedArtist[];
   available_markets: string[];
-  dics_number: number;
+  disc_number: number;
   duration_ms: number;
+  episode: boolean;
   explicit: boolean;
   external_urls: ExternalUrls;
   href: string;
   id: string;
+  is_local: boolean;
+  name: string;
+  track: boolean;
+  track_number: number;
   is_playable: boolean;
   linked_from: LinkedFrom;
   restrictions: Restrictions;
-  name: string;
-  track_number: number;
   type: string;
   uri: string;
-  is_local: boolean;
+};
+
+type SavedTrack = {
+  added_at: string;
+  track: Track;
 };
 
 type Track = SimplifiedTrack & {
@@ -307,7 +314,144 @@ type Track = SimplifiedTrack & {
   popularity: number;
 };
 
+type Tracks = {
+  tracks: Track[];
+};
+
 type TopTracksResult = {
+  tracks: Track[];
+};
+
+type AudioFeatures = {
+  acousticness: number;
+  analysis_url: string;
+  danceability: number;
+  duration_ms: number;
+  energy: number;
+  id: string;
+  instrumentalness: number;
+  key: number;
+  liveness: number;
+  loudness: number;
+  mode: number;
+  speechiness: number;
+  tempo: number;
+  time_signature: number;
+  track_href: string;
+  type: string;
+  uri: string;
+  valence: number;
+};
+
+type AudioFeaturesCollection = {
+  audio_features: AudioFeatures[];
+};
+
+type AudioAnalysis = {
+  meta: Meta;
+  track: TrackAnalysis;
+  bars: Bar[];
+  beats: Beat[];
+  sections: Section[];
+  segments: Segment[];
+  tatums: Tatum[];
+};
+
+type Meta = {
+  analyzer_version: string;
+  platform: string;
+  detailed_status: string;
+  status_code: number;
+  timestamp: number;
+  analysis_time: number;
+  input_process: number;
+};
+
+type TrackAnalysis = {
+  num_samples: number;
+  duration: number;
+  sample_md5: string;
+  offset_seconds: number;
+  window_seconds: number;
+  analysis_sample_rate: number;
+  analysis_channels: number;
+  end_of_fade_in: number;
+  end_of_fade_out: number;
+  loudness: number;
+  tempo: number;
+  tempo_confidence: number;
+  time_signature: number;
+  time_signature_confidence: number;
+  key: number;
+  key_confidence: number;
+  mode: number;
+  mode_confidence: number;
+  codestring: string;
+  code_version: number;
+  echoprintstring: string;
+  echoprint_version: number;
+  synchstring: string;
+  synch_version: number;
+  rhythmstring: string;
+  rhythm_version: number;
+};
+
+type Bar = {
+  start: number;
+  duration: number;
+  confidence: number;
+};
+
+type Beat = {
+  start: number;
+  duration: number;
+  confidence: number;
+};
+
+type Section = {
+  start: number;
+  duration: number;
+  confidence: number;
+  loudness: number;
+  tempo: number;
+  tempo_confidence: number;
+  key: number;
+  key_confidence: number;
+  mode: number;
+  mode_confidence: number;
+  time_signature: number;
+  time_signature_confidence: number;
+};
+
+type Segment = {
+  start: number;
+  duration: number;
+  confidence: number;
+  loudness_start: number;
+  loudness_max: number;
+  loudness_max_time: number;
+  loudness_end: number;
+  pitches: number[];
+  timbre: number[];
+};
+
+type Tatum = {
+  start: number;
+  duration: number;
+  confidence: number;
+};
+
+type RecommendationSeed = {
+  afterFilteringSize: number;
+  afterRelinkingSize: number;
+  href: string;
+  id: string;
+  initialPoolSize: number;
+  type: string;
+};
+
+type RecommendationsResponse = {
+  seeds: RecommendationSeed[];
   tracks: Track[];
 };
 
