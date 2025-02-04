dce4eac6bd3897a236c31635b427882bcfacb824
voffie <viktorsoderborg@gmail.com>
2025-02-04 23:18:16 +0100

    refactor: move fetch effect to separate function

--- src/services/ArtistService.ts
+++ src/services/ArtistService.ts
@@ -1,4 +1,4 @@
-import { IEntity } from "./EntityService.js";
+import { type IEntity, makeRequest } from "./EntityService.js";
 import type { Artist, Page, SimplifiedAlbum, Track } from "../types.js";
 import { Data, Effect } from "effect";
 
@@ -15,24 +15,9 @@ class ArtistService
    * @returns {Promise<Artist>} An artist
    */
   async get(artistId: string): Promise<Artist> {
+    Effect.log(`Requested artist with ID: ${artistId}`);
     return await Effect.runPromise(
-      Effect.tryPromise({
-        try: async () => {
-          const response = await fetch(
-            `https://api.spotify.com/v1/artists/${artistId}`,
-            {
-              headers: { Authorization: `Bearer ${this.apiKey}` },
-            },
-          );
-
-          if (!response.ok) {
-            throw new Error(`ArtistService Error: ${response.statusText}`);
-          }
-
-          return (await response.json()) as Artist;
-        },
-        catch: (error) => error as Error,
-      }),
+      makeRequest<Artist>(`artists/${artistId}`, this.apiKey),
     );
   }
 
@@ -45,24 +30,9 @@ class ArtistService
    * @returns {Promise<Artist[]>} A set of artists
    */
   async getMany(artistIds: string): Promise<Artist[]> {
+    Effect.log(`Requested artists with IDs: ${artistIds}`);
     return await Effect.runPromise(
-      Effect.tryPromise({
-        try: async () => {
-          const response = await fetch(
-            `https://api.spotify.com/v1/artists?${artistIds}`,
-            {
-              headers: { Authorization: `Bearer ${this.apiKey}` },
-            },
-          );
-
-          if (!response.ok) {
-            throw new Error(`ArtistService Error: ${response.statusText}`);
-          }
-
-          return (await response.json()) as Artist[];
-        },
-        catch: (error) => error as Error,
-      }),
+      makeRequest<Artist[]>(`artists?${artistIds}`, this.apiKey),
     );
   }
 
@@ -75,24 +45,12 @@ class ArtistService
    * @returns {Promise<Page<SimplifiedAlbum>>} Pages of albums
    */
   async getAlbums(artistId: string): Promise<Page<SimplifiedAlbum>> {
+    Effect.log(`Requested albums from artist with ID: ${artistId}`);
     return await Effect.runPromise(
-      Effect.tryPromise({
-        try: async () => {
-          const response = await fetch(
-            `https://api.spotify.com/v1/artists/${artistId}/albums`,
-            {
-              headers: { Authorization: `Bearer ${this.apiKey}` },
-            },
-          );
-
-          if (!response.ok) {
-            throw new Error(`ArtistService Error: ${response.statusText}`);
-          }
-
-          return (await response.json()) as Page<SimplifiedAlbum>;
-        },
-        catch: (error) => error as Error,
-      }),
+      makeRequest<Page<SimplifiedAlbum>>(
+        `artists/${artistId}/albums`,
+        this.apiKey,
+      ),
     );
   }
 
@@ -105,24 +63,9 @@ class ArtistService
    * @returns {Track[]} A set of tracks
    */
   async getTopTracks(artistId: string): Promise<Track[]> {
+    Effect.log(`Requested top tracks for artist with ID: ${artistId}`);
     return await Effect.runPromise(
-      Effect.tryPromise({
-        try: async () => {
-          const response = await fetch(
-            `https://api.spotify.com/v1/artists/${artistId}/top-tracks`,
-            {
-              headers: { Authorization: `Bearer ${this.apiKey}` },
-            },
-          );
-
-          if (!response.ok) {
-            throw new Error(`ArtistService Error: ${response.statusText}`);
-          }
-
-          return (await response.json()) as Track[];
-        },
-        catch: (error) => error as Error,
-      }),
+      makeRequest<Track[]>(`artists/${artistId}/top-tracks`, this.apiKey),
     );
   }
 
@@ -136,24 +79,9 @@ class ArtistService
    * @returns {Artist[]} A set of artists
    */
   async getRelatedArtists(artistId: string): Promise<Artist[]> {
+    Effect.log(`Requested related artists to artist with ID: ${artistId}`);
     return await Effect.runPromise(
-      Effect.tryPromise({
-        try: async () => {
-          const response = await fetch(
-            `https://api.spotify.com/v1/artists/${artistId}/related-artists`,
-            {
-              headers: { Authorization: `Bearer ${this.apiKey}` },
-            },
-          );
-
-          if (!response.ok) {
-            throw new Error(`ArtistService Error: ${response.statusText}`);
-          }
-
-          return (await response.json()) as Artist[];
-        },
-        catch: (error) => error as Error,
-      }),
+      makeRequest<Artist[]>(`artists/${artistId}/related-artists`, this.apiKey),
     );
   }
 }
