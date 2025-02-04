6ddf44ccd48f862f94944aa2da67122193756682
voffie <viktorsoderborg@gmail.com>
2025-02-04 23:18:16 +0100

    refactor: move fetch effect to separate function

--- src/services/EntityService.ts
+++ src/services/EntityService.ts
@@ -1,4 +1,39 @@
+import { Effect } from "effect";
+
 export interface IEntity<T> {
   get(id: string): Promise<T>;
   getMany(ids: string): Promise<T[]>;
+}
+
+export function makeRequest<T>(
+  route: string,
+  apiKey: string,
+): Effect.Effect<T, Error> {
+  return Effect.tryPromise({
+    try: async () => {
+      Effect.log(`Handling fetch for route: ${route}`);
+      const response = await fetch(`https://api.spotify.com/v1/${route}`, {
+        headers: { Authorization: `Bearer ${apiKey}` },
+      });
+
+      if (!response.ok) {
+        throw new Error(
+          `Error in services/makeRequest: ${response.statusText}`,
+        );
+      }
+
+      return (await response.json()) as T;
+    },
+    catch: (error) => {
+      const handledError = error as Error;
+      Effect.log(
+        `Unknown error while fetching route: ${route}`,
+        "Message:",
+        handledError.message,
+        "Trace:",
+        handledError.stack,
+      );
+      return handledError;
+    },
+  });
 }
