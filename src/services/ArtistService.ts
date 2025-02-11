5259ca3bf744ebfde680226951e1f28b2c5d5d9b
voffie <viktorsoderborg@gmail.com>
2025-02-11 16:20:55 +0100

    feat: rewrite EntityService

--- src/services/ArtistService.ts
+++ src/services/ArtistService.ts
@@ -1,9 +1,15 @@
+import {
+  ArtistSchema,
+  PageSchema,
+  SimplifiedAlbumSchema,
+  TrackSchema,
+} from "../schemas.js";
+import type { Artist, Page, SimplifiedAlbum, Track } from "../schemas.js";
 import { type IEntity, makeRequest } from "./EntityService.js";
-import type { Artist, Page, SimplifiedAlbum, Track } from "../types.js";
-import { Data, Effect } from "effect";
+import { Effect, Data, Schema } from "effect";
 
 class ArtistService
-  extends Data.TaggedClass("ArtistService")<{ readonly apiKey: string }>
+  extends Data.TaggedClass("ArtistService")
   implements IEntity<Artist>
 {
   /**
@@ -14,10 +20,11 @@ class ArtistService
    *
    * @returns {Promise<Artist>} An artist
    */
-  async get(artistId: string): Promise<Artist> {
-    Effect.log(`Requested artist with ID: ${artistId}`);
-    return await Effect.runPromise(
-      makeRequest<Artist>(`artists/${artistId}`, this.apiKey),
+  get(artistId: string): Promise<Artist> {
+    return Effect.runPromise(
+      Effect.gen(function* () {
+        return yield* makeRequest(`artists/${artistId}`, ArtistSchema);
+      }),
     );
   }
 
@@ -29,10 +36,14 @@ class ArtistService
    *
    * @returns {Promise<Artist[]>} A set of artists
    */
-  async getMany(artistIds: string): Promise<Artist[]> {
-    Effect.log(`Requested artists with IDs: ${artistIds}`);
-    return await Effect.runPromise(
-      makeRequest<Artist[]>(`artists?${artistIds}`, this.apiKey),
+  getMany(artistIds: string): Promise<Artist[]> {
+    return Effect.runPromise(
+      Effect.gen(function* () {
+        return yield* makeRequest(
+          `artists?${artistIds}`,
+          Schema.Array(ArtistSchema),
+        );
+      }),
     );
   }
 
@@ -44,13 +55,14 @@ class ArtistService
    *
    * @returns {Promise<Page<SimplifiedAlbum>>} Pages of albums
    */
-  async getAlbums(artistId: string): Promise<Page<SimplifiedAlbum>> {
-    Effect.log(`Requested albums from artist with ID: ${artistId}`);
-    return await Effect.runPromise(
-      makeRequest<Page<SimplifiedAlbum>>(
-        `artists/${artistId}/albums`,
-        this.apiKey,
-      ),
+  getAlbums(artistId: string): Promise<Page<SimplifiedAlbum>> {
+    return Effect.runPromise(
+      Effect.gen(function* () {
+        return yield* makeRequest(
+          `artists/${artistId}/albums`,
+          PageSchema(SimplifiedAlbumSchema),
+        );
+      }),
     );
   }
 
@@ -62,10 +74,14 @@ class ArtistService
    *
    * @returns {Track[]} A set of tracks
    */
-  async getTopTracks(artistId: string): Promise<Track[]> {
-    Effect.log(`Requested top tracks for artist with ID: ${artistId}`);
-    return await Effect.runPromise(
-      makeRequest<Track[]>(`artists/${artistId}/top-tracks`, this.apiKey),
+  getTopTracks(artistId: string): Promise<Track[]> {
+    return Effect.runPromise(
+      Effect.gen(function* () {
+        return yield* makeRequest(
+          `artists/${artistId}/top-tracks`,
+          Schema.Array(TrackSchema),
+        );
+      }),
     );
   }
 
@@ -78,10 +94,12 @@ class ArtistService
    *
    * @returns {Artist[]} A set of artists
    */
-  async getRelatedArtists(artistId: string): Promise<Artist[]> {
-    Effect.log(`Requested related artists to artist with ID: ${artistId}`);
-    return await Effect.runPromise(
-      makeRequest<Artist[]>(`artists/${artistId}/related-artists`, this.apiKey),
+  getRelatedArtists(artistId: string): Promise<Artist[]> {
+    return Effect.runPromise(
+      makeRequest(
+        `artists/${artistId}/related-artists`,
+        Schema.Array(ArtistSchema),
+      ),
     );
   }
 }
