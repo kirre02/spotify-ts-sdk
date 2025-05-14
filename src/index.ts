51e15b680417add771b0e06ed8b2d97bf49d06ae
voffie <viktorsoderborg@gmail.com>
2025-05-14 22:54:33 +0200

    fix: update index imports

--- src/index.ts
+++ src/index.ts
@@ -1,45 +1,46 @@
-import { AlbumService } from "./services/AlbumService";
-import { ArtistService } from "./services/ArtistService";
-import { AudiobookService } from "./services/AudiobookService";
-import { CategoryService } from "./services/CategoryService";
-import { ChapterService } from "./services/ChapterService";
-import { EpisodeService } from "./services/EpisodeService";
-import { MarketService } from "./services/MarketService";
-import { PlayerService } from "./services/PlayerService";
-import { PlaylistService } from "./services/PlaylistService";
-import { SearchService } from "./services/SearchService";
-import { ShowService } from "./services/ShowService";
-import { TrackService } from "./services/TrackService";
-import { UserService } from "./services/UserService";
+import { Schema } from "effect";
+import { AlbumService } from "@services/albums";
+import { ArtistService } from "@services/artists";
+import { AudiobookService } from "@services/audiobooks";
+import { CategoryService } from "@services/categories";
+import { ChapterService } from "@services/chapters";
+import { EpisodeService } from "@services/episodes";
+import { MarketService } from "@services/markets";
+import { PlayerService } from "@services/players";
+import { PlaylistService } from "@services/playlists";
+import { SearchService } from "@services/searches";
+import { ShowService } from "@services/shows";
+import { TrackService } from "@services/tracks";
+import { UserService } from "@services/users";
 
 interface PKCEAuth {
-  type: "PKCE";
-  clientId: string;
-  redirectUri: string;
+	type: "PKCE";
+	clientId: Schema.String;
+	redirectUri: Schema.String;
 }
 
 interface ClientCredentialsAuth {
-  type: "ClientCredentials";
-  clientId: string;
-  clientSecret: string;
+	type: "ClientCredentials";
+	clientId: Schema.String;
+	clientSecret: Schema.String;
 }
 
 type AuthStrategy = PKCEAuth | ClientCredentialsAuth;
 
 export function Client({ auth }: { auth: AuthStrategy }) {
-  return {
-    album: new AlbumService(),
-    artist: new ArtistService(),
-    audiobook: new AudiobookService(),
-    category: new CategoryService(),
-    chapter: new ChapterService(),
-    episode: new EpisodeService(),
-    market: new MarketService(),
-    player: new PlayerService(),
-    playlist: new PlaylistService(),
-    search: new SearchService(),
-    show: new ShowService(),
-    track: new TrackService(),
-    user: new UserService(),
-  };
+	return {
+		album: new AlbumService(),
+		artist: new ArtistService(),
+		audiobook: new AudiobookService(),
+		category: new CategoryService(),
+		chapter: new ChapterService(),
+		episode: new EpisodeService(),
+		market: new MarketService(),
+		player: new PlayerService(),
+		playlist: new PlaylistService(),
+		search: new SearchService(),
+		show: new ShowService(),
+		track: new TrackService(),
+		user: new UserService(),
+	};
 }
