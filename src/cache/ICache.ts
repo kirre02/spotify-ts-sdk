b360ac45baf63a6f69dc76cc10fc5fc03d53acba
voffie <viktorsoderborg@gmail.com>
2026-04-16 17:02:23 +0200

    refactor: remove unused cache files

--- src/cache/ICache.ts
+++
@@ -1,7 +0,0 @@
-import { Effect } from "effect";
-
-export default interface ICache {
-    get(key: string): Effect.Effect<string | undefined>
-    set(key: string, value: string): Effect.Effect<void>
-    remove(key: string): Effect.Effect<void>
-}
